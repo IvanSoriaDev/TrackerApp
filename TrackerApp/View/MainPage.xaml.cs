d5e19174e2d329435de84a1a4fc609ad313171a0
ivansoria <ivan.soria@outlook.com>
2023-06-18 13:15:48 -0500

    Basic MVVM implemented

---
+++ TrackerApp/View/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using TrackerApp.ViewModel;
+
+namespace TrackerApp.View;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage(MainViewModel vm)
+	{
+		InitializeComponent();
+		BindingContext = vm;
+	}
+}
---
+++ TrackerApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TrackerApp.ViewModel
+{
+	public partial class MainViewModel : ObservableObject
+	{
+		[ObservableProperty]
+		string text = "Demo content";
+	}
+}
+
